7e591d5e749296d72341f7856be98a9fbaea819b
NadirAmmor <93212487+NadirAmmor@users.noreply.github.com>
2022-12-09 20:21:31 +0100

    adding component Chef de Service and listing project and tasks

--- src/app/views/auth/login/login.component.ts
+++ src/app/views/auth/login/login.component.ts
@@ -1,20 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-import {AuthentificationService} from '../../../controller/service/authentification.service';
-import {User} from '../../../controller/model/user';
-import {Userauth} from '../../../controller/model/userauth';
-import {Router} from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { AuthentificationService } from "../../../controller/service/authentification.service";
+import { User } from "../../../controller/model/user";
+import { Userauth } from "../../../controller/model/userauth";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
+  selector: "app-login",
+  templateUrl: "./login.component.html",
 })
 export class LoginComponent implements OnInit {
-   userName='';
-  pass='';
+  userName = "";
+  pass = "";
   user: User = new User();
-  constructor(private auth: AuthentificationService,private router: Router) {}
+  constructor(private auth: AuthentificationService, private router: Router) {}
   ngOnInit(): void {
-    this.User=new User();
+    this.User = new User();
     this.UserAuth = new Userauth();
   }
 
@@ -33,26 +33,26 @@ export class LoginComponent implements OnInit {
     this.auth.UserAuth = value;
   }
 
-  SignIn(us :string,pass1:string) {
-    console.log('userna '+us+' paass '+pass1);
-    this.auth.Login(us,pass1).subscribe(
-      data => {
-        if (data != null) {
-          console.log('hna data : '+(data.body));
-        this.UserAuth =data.body;
-        this.User= this.UserAuth.user;
-          console.log('tken: '+ (this.UserAuth.accessToken) );
-          console.log('us: '+ JSON.stringify(this.User.role.name) );
-         // console.log(this.user.username)
-          if (this.UserAuth.accessToken != null){
+  SignIn(us: string, pass1: string) {
+    console.log("userna " + us + " paass " + pass1);
+    this.auth.Login(us, pass1).subscribe((data) => {
+      if (data != null) {
+        console.log("hna data : " + data.body);
+        this.UserAuth = data.body;
+        this.User = this.UserAuth.user;
+        console.log("tken: " + this.UserAuth.accessToken);
+        console.log("us: " + JSON.stringify(this.User.role.name));
+        // console.log(this.user.username)
+        if (this.UserAuth.accessToken != null) {
+          // tslint:disable-next-line:triple-equals
+          if (this.User.role.name == "ROLE_CHEF_SERVICE") {
+            this.router.navigate(["/chef"]);
             // tslint:disable-next-line:triple-equals
-            if (this.User.role.name == 'ROLE_DIRECTEUR'){
-              this.router.navigate(['/admin']);
-              // tslint:disable-next-line:triple-equals
-            }else if (this.User.role.name == 'ROLE_EMPLOIYE'){
-              this.router.navigate(['/emp']);
-            }
+          } else if (this.User.role.name == "ROLE_EMPLOIYE") {
+            this.router.navigate(["/emp"]);
           }
         }
-      })}
+      }
+    });
+  }
 }
