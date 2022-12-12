b2f98574c699a3594b5f3b0702190b4195945290
NadirAmmor <93212487+NadirAmmor@users.noreply.github.com>
2022-12-12 23:03:49 +0100

    update

--- src/app/views/ChefDeService/list-project-service/list-project-service.component.ts
+++ src/app/views/ChefDeService/list-project-service/list-project-service.component.ts
@@ -1,32 +1,28 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {EmployeService} from '../../../controller/service/employe.service';
-import {Project} from '../../../controller/model/project';
-import {Task} from '../../../controller/model/task';
-import {Router} from '@angular/router';
-import { ChefDeServiceService } from 'src/app/controller/service/chefDeService.service';
+import { Project } from "../../../controller/model/project";
+import { Task } from "../../../controller/model/task";
+import { Router } from "@angular/router";
+import { ChefDeServiceService } from "src/app/controller/service/chefDeService.service";
+import { Component, Input, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-list-project-service',
-  templateUrl: './list-project-service.component.html',
-  styleUrls: ['./list-project-service.component.css']
+  selector: "app-list-project-service",
+  templateUrl: "./list-project-service.component.html",
+  styleUrls: ["./list-project-service.component.css"],
 })
 export class ListProjectServiceComponent implements OnInit {
-
   @Input()
   get color(): string {
     return this._color;
   }
   set color(color: string) {
-    this._color = color !== 'light' && color !== 'dark' ? 'light' : color;
+    this._color = color !== "light" && color !== "dark" ? "light" : color;
   }
-  private _color = 'light';
+  private _color = "light";
 
-
-  constructor(private emp:ChefDeServiceService,private router: Router) {}
+  constructor(private emp: ChefDeServiceService, private router: Router) {}
   get ListProject(): Array<Project> {
     return this.emp.ListProject;
   }
-
 
   set ListProject(value: Array<Project>) {
     this.emp.ListProject = value;
@@ -46,26 +42,17 @@ export class ListProjectServiceComponent implements OnInit {
     this.emp.ListTask = value;
   }
 
-  public seeTask(id:string){
-    this.emp.getTaskProject(id).subscribe(data=>{
-      this.ListTask=data.body;
-      console.log( 'first'+ this.ListTask);
-      this.router.navigate(['/chef/projectTask']);
-    })
+  public seeTask(id: string) {
+    this.emp.getTaskProject(id).subscribe((data) => {
+      this.ListTask = data.body;
+      console.log("first" + this.ListTask);
+      this.router.navigate(["/chef/projectTask"]);
+    });
   }
   ngOnInit() {
-    this.emp.getAllChefDeProjetProject().subscribe(data=>{
-      this.ListProject=data.body;
+    this.emp.getAllChefDeProjetProject().subscribe((data) => {
+      this.ListProject = data.body;
       console.log(data);
-    })
+    });
   }
-
 }
-
-
-
-
-
-
-
-
